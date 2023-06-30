bae1a1ef1e285a5bfddae29314cd0c684be23d34
Brian Lee <brianlee1597@gmail.com>
2023-06-29 21:47:22 -0700

    actual

--- pages/api/get_todos.js
+++
@@ -1,8 +0,0 @@
-import makeshiftDB from '../../makeshift_db/memory_db';
-
-export default function handler (req, res) {
-  res.status(200).json({
-    message: "success",
-    todos: makeshiftDB.todos,
-  })
-}
